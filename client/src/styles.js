145dbc5db7b16f9401933d5d8ab293323413d8fb
SDivyaSravani <sankarasettydivyasravani.com>
2025-05-03 19:55:16 +0530

    Initial commit - MERN stack app

---
+++ client/src/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: '#2c3e50', // Dark slate blue
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
